bbe94b25605c09842ce6682e2722e45c152f433e
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2026-01-01 17:51:06 -0300

    Updating the number range in 'Mega-sena'

--- desafio_surpresinha.py
+++ desafio_surpresinha.py
@@ -16,10 +16,10 @@ while True:
         quant_jogos = int(input('Quantos jogos deseja fazer? '))
         if quant_jogos == 0:
             break
-        quant_num = int(input('Quantos números deseja escolher? [6 a 15] '))
-        while quant_num < 6 or quant_num > 15:
+        quant_num = int(input('Quantos números deseja escolher? [6 a 20] '))
+        while quant_num < 6 or quant_num > 20:
             print("Número inválido! Tente novamente.")
-            quant_num = int(input('Quantos números deseja escolher? [6 a 15] '))
+            quant_num = int(input('Quantos números deseja escolher? [6 a 20] '))
         for num in range(quant_jogos):
             num_unicos = random.sample(range(1, 61), quant_num)
             num_unicos.sort()
