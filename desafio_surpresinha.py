a35baf2c9066f31de6953ffde4b8522c4c7ee4c3
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2026-01-03 22:25:32 -0300

    Updating color 'quina'

--- desafio_surpresinha.py
+++ desafio_surpresinha.py
@@ -5,7 +5,7 @@ quant_num = 0
 while True:
     loteria = int(input('Qual loteria deseja apostar? (0 para parar)\n'
                         '\033[32mMega-sena [1]\n\033[m'
-                        'Quina [2]\n'
+                        '\033[34mQuina [2]\n\033[m'
                         'Lotofacil [3]\n'))
     if loteria == 0:
         break
