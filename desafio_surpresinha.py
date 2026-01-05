c30c95cb0562c6a011ac82d169f2ce8c0302e60d
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2026-01-04 22:43:34 -0300

    Creating a conditional 'Lotomania'

--- desafio_surpresinha.py
+++ desafio_surpresinha.py
@@ -6,10 +6,11 @@ while True:
     loteria = int(input('Qual loteria deseja apostar? (0 para parar)\n'
                         '\033[32mMega-sena [1]\n\033[m'
                         '\033[34mQuina [2]\n\033[m'
-                        '\033[35mLotofacil [3]\n\033[m'))
+                        '\033[35mLotofacil [3]\n\033[m'
+                        '\033[38;2;255;165;0mLotomania [4]\n\033[m'))
     if loteria == 0:
         break
-    if loteria < 0 or loteria > 3:
+    if loteria < 0 or loteria > 4:
         print('Opção inválida!')
     if loteria == 1: # Mega-sena
         print('Você escolheu \033[32mMega-sena\033[m')
@@ -53,6 +54,15 @@ while True:
             num_unicos.sort()
             print(num_unicos)
         print()
+    if loteria == 4: #Lotomania
+        print('Você escolheu \033[38;2;255;165;0mLotomania\033[m')
+        quant_jogos = int(input('Quantos jogos deseja fazer? '))
+        if quant_jogos == 0:
+            break
+        for num in range(quant_jogos):
+            num_unicos = random.sample(range(1, 101), 50)
+            num_unicos.sort()
+            print(num_unicos)
 
 #Ordenando os números em ordem crescente
 #num_unicos.sort()
