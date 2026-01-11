20a5e1d7e98bd69ecd481007ca1ee86625f08d77
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2026-01-10 21:40:11 -0300

    Updating message in 'lotofacil'

--- desafio_surpresinha.py
+++ desafio_surpresinha.py
@@ -54,7 +54,7 @@ while True:
             num_unicos = random.sample(range(1, 26), quant_num)
             num_unicos.sort()
             print(num_unicos)
-        print()
+        print('Boa sorte! 🍀🤞')
     if loteria == 4: #Lotomania
         print('Você escolheu \033[38;2;255;165;0mLotomania\033[m')
         quant_jogos = int(input('Quantos jogos deseja fazer? '))
