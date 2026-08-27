acdf4cba5ce8b124d6a1eca27e0e5d87afc06c25
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2026-08-27 01:40:08 -0300

    Renaming file "mega-sena.py" to "mega_sena.py" according to PEP8

---
+++ loterias/mega_sena.py
@@ -0,0 +1,18 @@
+def mega_sena():
+    while True:
+        print('Você escolheu \033[32mMega-sena\033[m')
+        quant_jogos = int(input('Quantos jogos deseja fazer? '))
+        if quant_jogos == 0:
+            break
+        quant_num = int(input('Quantos números deseja escolher? [6 a 20] '))
+        while quant_num < 6 or quant_num > 20:
+            print("Número inválido! Tente novamente.")
+            quant_num = int(input('Quantos números deseja escolher? [6 a 20] '))
+        for num in range(quant_jogos):
+            num_unicos = random.sample(range(1, 61), quant_num)
+            num_unicos.sort()
+            print(num_unicos)
+
+        print('\033[1;32;43mBoa sorte! 🍀🤞\033[m\n')
+
+mega_sena()
