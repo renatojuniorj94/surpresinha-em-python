8aed4a5f3ff7dd9cb9677547a5d664a1754e3745
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2026-09-10 14:27:48 -0300

    Printing set operations

--- tabela-clientes.py
+++ tabela-clientes.py
@@ -5,3 +5,8 @@ interseção = clientes_produto_x & clientes_produto_y
 apenas_x = clientes_produto_x - clientes_produto_y
 apenas_y = clientes_produto_y - clientes_produto_x
 todos_clientes = clientes_produto_x | clientes_produto_y
+
+print(f'Cliente que usam os dois produtos ({interseção})')
+print(f'Cliente que usam apenas o produto X ({apenas_x})')
+print(f'Cliente que usam apenas o produto Y ({apenas_y})')
+print(f'Todos os clientes ({todos_clientes})')
