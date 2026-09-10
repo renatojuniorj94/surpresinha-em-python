ce1d76dd24fceb01867a6fac260eaff359fe18ab
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2026-09-10 14:41:05 -0300

    Inserting comments

--- tabela-clientes.py
+++ tabela-clientes.py
@@ -1,10 +1,10 @@
 clientes_produto_x = {'c1', 'c2', 'c3', 'c4', 'c5', 'c6'}
 clientes_produto_y = {'c4', 'c5', 'c7', 'c8'}
 
-interseção = clientes_produto_x & clientes_produto_y
-apenas_x = clientes_produto_x - clientes_produto_y
-apenas_y = clientes_produto_y - clientes_produto_x
-todos_clientes = clientes_produto_x | clientes_produto_y
+interseção = clientes_produto_x & clientes_produto_y  # Interseção (A ∩ B)
+apenas_x = clientes_produto_x - clientes_produto_y  # Diferença (A \ B)
+apenas_y = clientes_produto_y - clientes_produto_x  # Diferença (B \ A)
+todos_clientes = clientes_produto_x | clientes_produto_y  # União (A ∪ B)
 
 print(f'Cliente que usam os dois produtos ({interseção})')
 print(f'Cliente que usam apenas o produto X ({apenas_x})')
