462f3784d6b381a99a2b9586732501556125d14c
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2026-09-10 14:26:29 -0300

    Creating operations for sets

--- tabela-clientes.py
+++ tabela-clientes.py
@@ -1,3 +1,7 @@
 clientes_produto_x = {'c1', 'c2', 'c3', 'c4', 'c5', 'c6'}
 clientes_produto_y = {'c4', 'c5', 'c7', 'c8'}
 
+interseção = clientes_produto_x & clientes_produto_y
+apenas_x = clientes_produto_x - clientes_produto_y
+apenas_y = clientes_produto_y - clientes_produto_x
+todos_clientes = clientes_produto_x | clientes_produto_y
